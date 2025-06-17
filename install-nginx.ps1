6862561aa65ed5d6a55103a7f06712981c468954
jensdiedrich <58627248+jensdiedrich@users.noreply.github.com>
2025-06-17 15:49:50 +0200

    Update install-nginx.ps1

--- install-nginx.ps1
+++ install-nginx.ps1
@@ -116,8 +116,8 @@ http {
         location / {
             proxy_bind  $FirstIp;
             proxy_pass https://ifconfig.me;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For `$proxy_add_x_forwarded_for;
+            proxy_set_header X-Real-IP `$remote_addr;
         }
 		
     }
