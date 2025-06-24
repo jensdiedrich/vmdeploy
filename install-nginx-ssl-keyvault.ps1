ff9362311d4aad5f9fecda3c2838da05f65c2c5b
jensdiedrich <58627248+jensdiedrich@users.noreply.github.com>
2025-06-24 17:27:42 +0200

    Update install-nginx-ssl-keyvault.ps1

--- install-nginx-ssl-keyvault.ps1
+++ install-nginx-ssl-keyvault.ps1
@@ -84,9 +84,6 @@ Expand-Archive e:\simple-acme-keyvault-plugin.zip e:\simple-acme
 New-Item -ItemType Directory -Path e:\nginx\conf\ssl
 New-Item -ItemType Directory -Path e:\nginx\html\.well-known
 
-# e:\simple-acme\wacs.exe --baseuri https://acme-staging-v02.api.letsencrypt.org/directory --verbose --accepttos --emailaddress noreply@hh-software.com --source manual --host $hostname --validation filesystem --webroot e:\nginx\html --store pemfiles --pemfilespath e:\nginx\conf\ssl --pemfilesname $hostname
-e:\simple-acme\wacs.exe --verbose --accepttos --emailaddress noreply@hh-software.com --source manual --host $hostname --validation filesystem --webroot e:\nginx\html --store pemfiles --pemfilespath e:\nginx\conf\ssl --pemfilesname $hostname
-
 e:\simple-acme\wacs.exe --baseuri https://acme-staging-v02.api.letsencrypt.org/directory --verbose --accepttos --emailaddress noreply@hh-software.com --source manual `
  --host $hostname --validation filesystem --webroot e:\nginx\html --store pemfiles,keyvault --pemfilespath e:\nginx\conf\ssl --pemfilesname $hostname `
 --vaultname $keyVaultName --certificatename $hostname.Replace('.','-') --azuretenantid $tenantId --azureusemsi
