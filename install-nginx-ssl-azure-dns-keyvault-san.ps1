d5dc2ed3c86cd895d7862a0b915143f39a69e28a
jensdiedrich <58627248+jensdiedrich@users.noreply.github.com>
2025-09-03 17:15:22 +0200

    set delay to 5 minutes

--- install-nginx-ssl-azure-dns-keyvault-san.ps1
+++ install-nginx-ssl-azure-dns-keyvault-san.ps1
@@ -17,9 +17,9 @@ param(
 )
 
 # Delay if script runs in parallel on multiple vms
-"delay: $($delay * 120)" | out-file -append c:\temp\delay.txt
+"delay: $($delay * 500)" | out-file -append c:\temp\delay.txt
 "before delay: $(get-date)" | out-file -append c:\temp\delay.txt
-Start-Sleep ($delay * 120)
+Start-Sleep ($delay * 500)
 "after delay: $(get-date)" | out-file -append c:\temp\delay.txt
 
 Start-Transcript -Path "e:\install-nginx.log" -Append
